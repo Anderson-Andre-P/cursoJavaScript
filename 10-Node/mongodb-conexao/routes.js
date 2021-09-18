fbafb5203a9aaa70778fd6b6ec9103b280884d8c
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-09-18 13:52:55 -0300

    update nome de pasta

---
+++ 10-Node/mongodb-conexao/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const route = express.Router()
+const homeController = require('./src/controllers/homeController')
+const contatoController = require('./src/controllers/contatoController')
+
+function meuMiddleware(req, res, next) {
+    req.session = { nome: 'Anderson', sobrenome:'Pereira' }
+    console.log()
+    console.log('Passei no seu middleware.')
+    console.log()
+    next()
+}
+
+route.get('/', meuMiddleware, homeController.paginaInicial, function(req, res) {
+    console.log('Ainda estou aqui.')
+    console.log(`'Último middleware' olha o que rem na req.session.nome ${req.session.nome}`)
+});
+
+route.post('/', homeController.trataPost)
+
+route.get('/contato', contatoController.paginaInicial)
+
+module.exports = route;
